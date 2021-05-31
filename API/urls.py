ccf3ab0756847fda2f4248c96029e27048082247
Ryan <75308402+Ryan-L98@users.noreply.github.com>
2021-05-31 19:11:15 +0800

    test commit

--- API/urls.py
+++ API/urls.py
@@ -5,6 +5,4 @@ urlpatterns = [
     path('recipes', views.recipeList.as_view(), name='recipe-list'),
     path('recipes/<pk>/', views.recipeDetails.as_view(), name='recipe-details'),
     path('<username>/calories/', views.calorieView.as_view(), name='calories'),
-    #path('signup', views.userCreate.as_view(), name='signup'),
-
 ]
